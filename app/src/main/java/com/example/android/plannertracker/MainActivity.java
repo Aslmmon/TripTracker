4c9a66db80f102cec3281465a21f944991d96e8b
=Aslm <=>
2019-02-18 20:18:34 +0200

    ok2

--- app/src/main/java/com/example/android/plannertracker/MainActivity.java
+++ app/src/main/java/com/example/android/plannertracker/MainActivity.java
@@ -27,7 +27,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    public static final String TRIP_DATAA = "Trip Data";
+    public static final String TRIP_DATA = "Trip Data";
 
 
     DatabaseReference databaseReference;
